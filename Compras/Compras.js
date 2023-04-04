af6114e1b7726b14b5177ce4c532cde4886f420d
Avier-10 <97894530+Avier-10@users.noreply.github.com>
2023-04-04 15:39:37 -0300

    Se arreglo un error y se termino el Filtro

--- Compras/Compras.js
+++ Compras/Compras.js
@@ -1,67 +1,83 @@
-window.onload = function GeneradorDeCajas() {
-  var DatosRopa = [
-    {
-      Titulo: "Título 1",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
-      Tipo: "Pantalon",
-    },
-    {
-      Titulo: "Título 2",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
-      Tipo: "Buzo",
-    },
-    {
-      Titulo: "Título 3",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
-      Tipo: "Pantalon",
-    },
-    {
-      Titulo: "Título 4",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
-      Tipo: "Buzo",
-    },
-    {
-      Titulo: "Título 5",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
-      Tipo: "Pantalon",
-    },
-    {
-      Titulo: "Título 6",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
-      Tipo: "Buzo",
-    },
-    {
-      Titulo: "Título 7",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
-      Tipo: "Pantalon",
-    },
-    {
-      Titulo: "Título 8",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
-      Tipo: "Buzo",
-    },
-    {
-      Titulo: "Título 9",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
-      Tipo: "Pantalon",
-    },
-    {
-      Titulo: "Título 10",
-      Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
-      Tipo: "Buzo",
-    },
-  ];
+var E;
+var DatosRopa_Pantalon = [];
+var DatosRopa_Buzo = [];
+var DatosRopa_Inicial = [
+  {
+    Titulo: "Título 1",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
+    Tipo: "Pantalon",
+  },
+  {
+    Titulo: "Título 2",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
+    Tipo: "Buzo",
+  },
+  {
+    Titulo: "Título 3",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
+    Tipo: "Pantalon",
+  },
+  {
+    Titulo: "Título 4",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
+    Tipo: "Buzo",
+  },
+  {
+    Titulo: "Título 5",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
+    Tipo: "Pantalon",
+  },
+  {
+    Titulo: "Título 6",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
+    Tipo: "Buzo",
+  },
+  {
+    Titulo: "Título 7",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
+    Tipo: "Pantalon",
+  },
+  {
+    Titulo: "Título 8",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
+    Tipo: "Buzo",
+  },
+  {
+    Titulo: "Título 9",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-1.jpg",
+    Tipo: "Pantalon",
+  },
+  {
+    Titulo: "Título 10",
+    Imagen: "/TrabajoIntegradorUTN/img/Imagen-Deportiva-2.jpg",
+    Tipo: "Buzo",
+  },
+];
+var DatosRopa = [DatosRopa_Inicial, DatosRopa_Pantalon, DatosRopa_Buzo];
 
+function FiltPantalon() {
+  DatosRopa_Pantalon = DatosRopa_Inicial.filter(
+    (Tip) => Tip.Tipo === "Pantalon"
+  );
+  E = 1;
+  DatosRopa[1] = DatosRopa_Pantalon;
+  GeneradorDeCajas(E);
+}
+function FiltBuzo() {
+  DatosRopa_Buzo = DatosRopa_Inicial.filter((Tip) => Tip.Tipo === "Buzo");
+  E = 2;
+  DatosRopa[2] = DatosRopa_Buzo;
+  GeneradorDeCajas(E);
+}
+
+function GeneradorDeCajas(E) {
   var Columna = 1;
   var Fila = 2;
+  var i = 0;
 
-  function FiltPantalon() {
-    for (j = 0; j < DatosRopa.length; j++) {
-      DatosRopa.filter(DatosRopa[j].Tipo === "Pantalon");
-    }
-  }
-
-  for (i = 0; i < DatosRopa.length; i++) {
+  for (i; i < DatosRopa[E].length; i++) {
+    console.log(DatosRopa);
+    
     //Creacion De Cajas Dinamico
     const Caja = document.createElement("div");
     Caja.id = "Caja" + [i];
@@ -77,7 +93,7 @@ window.onload = function GeneradorDeCajas() {
     const Img = document.createElement("img");
     Caja.appendChild(Img);
     Img.className = "EstiloImg";
-    Img.src = DatosRopa[i].Imagen;
+    Img.src = DatosRopa[E][i].Imagen;
 
     if (Columna < 4) {
       Columna++;
@@ -90,4 +106,4 @@ window.onload = function GeneradorDeCajas() {
       Caja.style.gridRow = Fila;
     }
   }
-};
+}
