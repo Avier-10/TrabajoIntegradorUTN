08b0d83536b9fee8ac28f0687d37544c5b594e9d
victoriaallendee <110253004+victoriaallendee@users.noreply.github.com>
2023-04-16 22:10:18 -0300

    formulario

---
+++ Formulario de contacto/validar.js
@@ -0,0 +1,26 @@
+var nombre =document.getElementById("nombre");
+var apellido =document.getElementById("apellido");
+var email =document.getElementById("email");
+var telefono =document.getElementById("telefono");
+function enviarFormulario(){
+console.log("Enviado")
+
+if(nombre.value === "null" || nombre.value === ""){
+    alert("Hay campos incompletos ")
+}else{
+    if(apellido.value === "null" || apellido.value === ""){
+        alert("Hay campos incompletos ")
+        
+    }else{
+        if(email.value === "null" || email.value === ""){
+            alert("Hay campos incompletos")
+        }else{
+            alert("Se envio correctamente")
+        }
+    }
+}
+
+return false
+   
+
+}
